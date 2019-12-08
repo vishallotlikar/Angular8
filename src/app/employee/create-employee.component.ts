1005efa94dc24ceb0c308e1ef18054067db59388
vishal lotlikar <lotlikar18@gmail.com>
2019-12-08 19:41:24 +0530

    reactive form

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.scss']
 })
 export class CreateEmployeeComponent implements OnInit {
+  employeeForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.employeeForm = new FormGroup({
+      fullName: new FormControl(),
+      email: new FormControl()
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.employeeForm.value)
   }
 
 }
