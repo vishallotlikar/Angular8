ce96facf4a6176f0057cbf14990c913aef3dd740
vishal lotlikar <lotlikar18@gmail.com>
2019-12-08 18:43:53 +0530

    First angular8 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular8Project';
+  title = 'My first Angular8Project';
 }
