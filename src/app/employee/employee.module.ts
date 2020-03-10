1ad20da40033fcfcda52433b57fb369f5c214e4d
vishal lotlikar <lotlikar18@gmail.com>
2020-03-10 13:15:09 +0530

    Create feature module in angular

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { CreateEmployeeComponent } from './create-employee.component';
+import { ListEmployeesComponent } from './list-employees.component';
+
+@NgModule({
+  declarations: [
+    CreateEmployeeComponent,
+    ListEmployeesComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class EmployeeModule { }
